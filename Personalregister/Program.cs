d6df3f49c766c5f92f2bdedf07129ca868c9b95f
Hamredahl <JHamredahl@gmail.com>
2025-04-01 15:53:10 +0200

    Lite trevligare gui och exceptions vid felaktig add.

--- Personalregister/Program.cs
+++ Personalregister/Program.cs
@@ -5,24 +5,29 @@
         static List<object> register = new List<object> ();
         static void Main(string[] args)
         {
-            Console.WriteLine("Välkommen till ett simpelt personalregister! \n\nFör att lägga till personal, skriv: add:'fullständigt namn':'lön' \nFör att skriva ut personalregister, skriv: print \nFör att avsluta programmet, skriv: exit\n" );
+            Console.WriteLine("Välkommen till ett simpelt personalregister! \nSkriv help för en lista med kommandon.\n" );
             bool running = true;
             while (running) 
             {
-                string[] input = Console.ReadLine().Split(':');
-                switch (input[0])
+                string input = Console.ReadLine();
+                switch (input)
                 {
+                    case "help":
+                        Console.WriteLine("\nKommandolista:\nadd\n   Lägg till ny personal.\nprint\n   Skriv ut personalregistret.\nclear\n   Rensa konsolen på tidigare text.\nexit\n   Avsluta programmet.\n");
+                        break;
                     case "add":
-                        try
-                        {
-                            addEmployee(input[1], Convert.ToInt32(input[2]));
-                        } catch
-                        {
-                            Console.WriteLine("Kunde inte skapa personal.");
-                        }
+                            Console.Write("Personens namn: ");
+                            string name = Console.ReadLine();
+                            Console.Write("Personens lön: ");
+                            string salary = Console.ReadLine();
+                            addEmployee(name, salary);
                         break;
                     case "print":
                         printRegister();
+                        break;
+                    case "clear":
+                        Console.Clear();
+                        Console.WriteLine("Välkommen till ett simpelt personalregister! \nSkriv help för en lista med kommandon.\n");
                         break;
                     case "exit":
                         running = false;
@@ -34,21 +39,40 @@
             }
         }
 
-        public static void addEmployee(string name, int salary)
+        public static void addEmployee(string name, string salary)
         {
-            Employee employee = new Employee(name, salary); 
-            register.Add(employee);
-            Console.WriteLine(employee.name + " tillagd i registret med " + employee.salary + " i lön. \n");
+            try
+            {
+                if (name.Length < 1) {throw new ArgumentOutOfRangeException(nameof(name));}
+                Employee employee = new Employee(name, Convert.ToInt32(salary));
+                register.Add(employee);
+                Console.WriteLine(employee.name + " tillagd i registret med " + employee.salary + " i lön. \n");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Kunde ej skapa personal:\n   Namn får ej vara tomt.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Kunde ej skapa personal:\n   Lön måste vara ett heltal.");
+            }
         }
 
         public static void printRegister()
         {
-            Console.WriteLine("\nPersonalregister:");
-            foreach (Employee employee in register)
-            {
-                Console.WriteLine(employee.name + ", " + employee.salary);
+            if (register.Count == 0) 
+            { 
+                Console.WriteLine("Det finns ingen personal i registret ännu."); 
             }
-            Console.WriteLine("");
+            else
+            {
+                Console.WriteLine("\nPersonalregister:");
+                foreach (Employee employee in register)
+                {
+                    Console.WriteLine(employee.name + ", " + employee.salary);
+                }
+                Console.WriteLine("");
+            }
         }
     }
 
